e3ed6ac80a606b81562f8b58720e7c1248730bee
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-16 14:03:39 -0300

    Update AuthContext.js

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,1 +1,47 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth, googleProvider } from "../firebaseConfig";
+import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Função para abrir o Pop-up do Google
+  function loginGoogle() {
+    return signInWithPopup(auth, googleProvider);
+  }
+
+  // Função de Sair (Logout)
+  function logout() {
+    return signOut(auth);
+  }
+
+  // O "Vigia": monitora se o usuário entrou ou saiu
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    loginGoogle,
+    logout
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
 
