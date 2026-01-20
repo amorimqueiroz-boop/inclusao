a248e9159d2485fce4b05b814a175d169f2d3d7c
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-20 18:00:23 -0300

    Update 2_PAE.py

--- pages/2_PAE.py
+++ pages/2_PAE.py
@@ -1,12 +1,7 @@
 import streamlit as st
 from ui_nav import render_omnisfera_nav
 
-st.set_page_config(page_title="Omnisfera | PAEE", layout="wide")
-
 render_omnisfera_nav(active="paee")
-
-
-render_topbar_nav(active="paee")
 
 st.title("PAEE — teste mínimo")
 st.success("Menu carregou aqui ✅")
