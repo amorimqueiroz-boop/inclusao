832a742034a6360a9ca2a807517646f2cbc86dff
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-20 17:59:26 -0300

    Update home_portal.py

--- home_portal.py
+++ home_portal.py
@@ -2,12 +2,10 @@
 import streamlit as st
 import base64
 import os
+from ui_nav import render_omnisfera_nav
+render_omnisfera_nav(active="home")
 import streamlit as st
 from ui_nav import render_omnisfera_nav
-
-st.set_page_config(page_title="Omnisfera | PAEE", layout="wide")
-
-render_omnisfera_nav(active="paee")
 
 # =========================================================
 # CONFIG
