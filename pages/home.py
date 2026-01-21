c957fd5c3b715051cc46748ccae63dba0490a7d2
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-21 11:30:21 -0300

    Delete pages/home.py

--- pages/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from ui_nav import boot_ui, ensure_auth_state
-
-ensure_auth_state()
-boot_ui()
-
-st.title("Home OK")
-st.write("Se você está vendo isso, o arquivo está limpo.")
